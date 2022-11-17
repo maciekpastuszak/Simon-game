55af62854b766a2c91c1f2b20c7af597729934bd
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-11-17 09:04:19 +0100

    add animate press and refactor

--- game.js
+++ game.js
@@ -1,26 +1,36 @@
+
+let buttonColours =["red", "blue", "green", "yellow"];
+
 let gamePattern = [];
 let userClickedPattern = [];
-let buttonColours =["red", "blue", "green", "yellow"];
+
+$(".btn").on("click", function(){
+    var userChoosenColor = $(this).attr("id");
+    userClickedPattern.push(userChoosenColor);
+    playSound(userChoosenColor);
+});
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+      }, 100);
+}
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour)
 }
 nextSequence()
 
-
-$(".btn").on("click", function(){
-    var userChoosenColor = $(this).attr("id");
-    $(this).fadeOut(100).fadeIn(100);
-    var audio = new Audio("/sounds/" + userChoosenColor + ".mp3");
-    audio.play();
-    userClickedPattern.push(userChoosenColor);
-    console.log(userClickedPattern);
-});
-
 function playSound(name){
-    
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
 
